49a956f539f3700fca90ade8def9a5a2e8d47959
Pablo Barberá <pablo.barbera@nyu.edu>
2015-02-08 14:40:09 -0500

    added NSF award mention

---
+++ R/utils.R
@@ -0,0 +1,12 @@
+.onAttach <- function(...) {
+ 
+   packageStartupMessage("##\n## smappR: tools for analysis of Twitter data")
+   packageStartupMessage("## Social Media and Political Participation Lab at NYU")
+   packageStartupMessage("## smapp.nyu.edu @SMAPP_NYU")
+   packageStartupMessage("##\n## Support provided by the U.S. National Science Foundation")
+   packageStartupMessage("## (INSPIRE program award #1248077)\n##")
+
+}
+
+
+
